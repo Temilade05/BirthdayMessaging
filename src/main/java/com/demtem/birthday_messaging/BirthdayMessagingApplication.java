06502fcbc252419ac7237535f015c32957ac6fe9
Temilade05 <temioladugba@gmail.com>
2020-07-17 15:34:51 +0100

    Initial commit

---
+++ src/main/java/com/demtem/birthday_messaging/BirthdayMessagingApplication.java
@@ -0,0 +1,13 @@
+package com.demtem.birthday_messaging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirthdayMessagingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirthdayMessagingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demtem/birthday_messaging/BirthdayMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demtem.birthday_messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirthdayMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
